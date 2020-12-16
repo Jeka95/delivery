af7dc541215d3f13dfb408f0f3d3d6a4927e1604
Jeka95 <72086251+Jeka95@users.noreply.github.com>
2020-12-17 01:49:05 +0200

    64%

--- src/containers/Footer/index.jsx
+++ src/containers/Footer/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
-
-import Info from '../../components/Info';
 import "./index.scss";
 
 const Footer = () => {
    return (
       <footer className="footer">
-         <div>всі права захищені</div>
-         <Info />
+         <div className="footer__name">2020 React Delivery</div>
+         <div className="footer__lincs">
+            <Link to="/contacts" className="footer__linc" >Контакти</Link>
+            <Link to="/about" className="footer__linc" >Про нас</Link>
+            <Link to="/howtoorder" className="footer__linc" >Як замовити</Link>
+
+         </div>
       </footer>
    );
 }
